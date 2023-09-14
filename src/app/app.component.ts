a094566c18d7ae1ea20d0350c4855c03b9c3e773
micbuffa <micbuffa@gmail.com>
2023-09-14 09:22:38 +0200

    premier commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'assignment-app';
+  titre = 'Application de gestion de devoirs';
+  nomProf = 'Michel Buffa';
+  x = 3;
 }
